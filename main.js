7efee8d2e7278039169ef56593311a6b70fdc9d4
Dave Spina <dspina79@gmail.com>
2021-06-24 20:58:46 -0400

    Added proof of work

--- main.js
+++ main.js
@@ -7,17 +7,29 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.ts + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.ts + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
+
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
@@ -25,7 +37,8 @@ class Blockchain {
 
     addBlock(block) {
         block.previousHash = this.getLatestBlock().hash;
-        block.hash = block.calculateHash();
+        // add the block by mining it
+        block.mineBlock(this.difficulty);
         this.chain.push(block);
     }
 
