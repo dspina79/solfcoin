b8ae61d57c009e3edb7ede2e52fbaeca21e12366
Dave Spina <dspina79@gmail.com>
2021-06-29 20:45:22 -0400

    Added signature capability to blockchain transaction

--- blockhain.js
+++ blockhain.js
@@ -1,11 +1,38 @@
 const SHA256 = require('crypto-js/sha256');
 const fs = require('fs');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaction {
     constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey) {
+        if (signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You cannot sign a hash from a different wallet.');
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+        this.signature = signature.toDER('hex');
+    }
+
+    isValid() {
+        if (this.fromAddress === null) return true;
+
+        if (!this.signature || this.signature.length === 0) {
+            throw new Error('The signature is empty!');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        publicKey.verify(this.calculateHash(), this.signature);
     }
 }
 
